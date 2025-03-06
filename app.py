83a9f9b6df73fb9471310aa60db480c60d6e7896
DataVuong <tiger.data105@gmail.com>
2025-03-06 22:07:48 +0700

    Cập nhật thêm đăng kí, đăng nhập

--- app.py
+++ app.py
@@ -1,23 +1,62 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-# Danh sách bài blog (giả lập)
-blogs = [
-    {"title": "Bài viết đầu tiên", "content": "Nội dung bài viết 1"},
-    {"title": "Bài viết thứ hai", "content": "Nội dung bài viết 2"}
-]
+# Model User
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
-# Danh sách việc cần làm (giả lập)
-todos = ["Học Flask", "Viết code", "Push lên GitHub"]
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
+# Routes
 @app.route('/')
 def home():
-    return render_template('index.html', blogs=blogs)
+    posts = [{"title": "Bài viết 1", "content": "Nội dung bài viết 1"}]
+    return render_template('index.html', posts=posts)
 
-@app.route('/todo')
-def todo():
-    return render_template('todo.html', todos=todos)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and user.password == request.form['password']:
+            login_user(user)
+            return redirect(url_for('home'))
+        flash('Sai tên đăng nhập hoặc mật khẩu!')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if User.query.filter_by(username=username).first():
+            flash('Tên đăng nhập đã tồn tại!')
+        else:
+            user = User(username=username, password=password)
+            db.session.add(user)
+            db.session.commit()
+            flash('Đăng ký thành công!')
+            return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
